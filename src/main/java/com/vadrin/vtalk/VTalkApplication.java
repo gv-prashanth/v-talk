d9dbbfd8a935606ba6e8fb5199c9f8fb51344e79
Prashanth Venkat Gumma <pragumma0@publicisgroupe.net>
2018-11-22 18:26:48 +0530

    initial commit

---
+++ src/main/java/com/vadrin/vtalk/VTalkApplication.java
@@ -0,0 +1,12 @@
+package com.vadrin.vtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VTalkApplication.class, args);
+	}
+}
