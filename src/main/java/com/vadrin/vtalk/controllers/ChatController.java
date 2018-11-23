b219df7155bcc6d647a7a05bd6bbf083940283c3
Prashanth Venkat Gumma <pragumma0@publicisgroupe.net>
2018-11-23 19:32:08 +0530

    controller for chat.

---
+++ src/main/java/com/vadrin/vtalk/controllers/ChatController.java
@@ -0,0 +1,29 @@
+package com.vadrin.vtalk.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+	
+	private String lastChat = "Welcome to V-Talk!";
+	private int chatline = 1;
+	
+    @GetMapping("/receive")
+    public String receive() {
+        return lastChat;
+    }
+    
+    @GetMapping("/send")
+    public void send(@RequestParam(name="inptchat", required=true) String inptchat) {
+        this.lastChat = inptchat;
+        this.chatline++;
+    }
+    
+    @GetMapping("/chatline")
+    public int chatline() {
+        return chatline;
+    }   
+
+}
