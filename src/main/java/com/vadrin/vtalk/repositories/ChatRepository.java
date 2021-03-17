61059e73ad793b7e72cfd11b51f1238a4d240fb1
Gumma <3628612@lowes.com>
2021-03-17 10:56:45 +0530

    reverting

--- src/main/java/com/vadrin/vtalk/repositories/ChatRepository.java
+++ src/main/java/com/vadrin/vtalk/repositories/ChatRepository.java
@@ -12,8 +12,7 @@ import com.vadrin.vtalk.models.Chat;
 @Repository
 public interface ChatRepository extends JpaRepository<Chat, Integer> {
 
-  //@Query(nativeQuery = true, value = "select * from chats where sender in ?1 and receiver in ?1 and created_on > (select created_on from chats where receiver=?3 and sender=?2 and created_on <= (select created_on from chats where receiver=?2 and sender=?3 order by created_on desc limit 1) order by created_on desc limit 1) order by created_on asc")
-  @Query(nativeQuery = true, value = "select * from chats where sender in ?1 and receiver in ?1 and created_on > '2021-03-16 16:51:49' order by created_on asc")
+  @Query(nativeQuery = true, value = "select * from chats where sender in ?1 and receiver in ?1 and created_on > (select created_on from chats where receiver=?3 and sender=?2 and created_on <= (select created_on from chats where receiver=?2 and sender=?3 order by created_on desc limit 1) order by created_on desc limit 1) order by created_on asc")
   public List<Chat> findLatestChats(Set<String> combined, String sender, String receiver);
 
 }
