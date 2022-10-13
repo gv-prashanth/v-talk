39d4b47c28ef73305955d4bb21fde0be7b8674a5
GV Prashanth <36256971+gv-prashanth@users.noreply.github.com>
2022-10-14 00:16:28 +0530

    Testing documentation approach for db connection

--- src/main/java/com/vadrin/vtalk/configurations/PostgreConfig.java
+++ src/main/java/com/vadrin/vtalk/configurations/PostgreConfig.java
@@ -9,20 +9,20 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PostgreConfig {
-  @Bean
-  public BasicDataSource dataSource() throws URISyntaxException {
-    URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-    String username = dbUri.getUserInfo().split(":")[0];
-    String password = dbUri.getUserInfo().split(":")[1];
-    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath()
-        + "?sslmode=require";
-
-    BasicDataSource basicDataSource = new BasicDataSource();
-    basicDataSource.setUrl(dbUrl);
-    basicDataSource.setUsername(username);
-    basicDataSource.setPassword(password);
-
-    return basicDataSource;
-  }
+//  @Bean
+//  public BasicDataSource dataSource() throws URISyntaxException {
+//    URI dbUri = new URI(System.getenv("DATABASE_URL"));
+//
+//    String username = dbUri.getUserInfo().split(":")[0];
+//    String password = dbUri.getUserInfo().split(":")[1];
+//    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath()
+//        + "?sslmode=require";
+//
+//    BasicDataSource basicDataSource = new BasicDataSource();
+//    basicDataSource.setUrl(dbUrl);
+//    basicDataSource.setUsername(username);
+//    basicDataSource.setPassword(password);
+//
+//    return basicDataSource;
+//  }
 }
