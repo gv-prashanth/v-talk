80e93126c3d7e58dca170e4fa643984140ba11c3
GV Prashanth <36256971+gv-prashanth@users.noreply.github.com>
2022-11-24 18:03:00 +0530

    Update WBLabourNotificationService.java

--- src/main/java/com/vadrin/vtalk/services/notifications/WBLabourNotificationService.java
+++ src/main/java/com/vadrin/vtalk/services/notifications/WBLabourNotificationService.java
@@ -22,7 +22,7 @@ import com.vadrin.vtalk.repositories.UserInfoRepository;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Service
+//@Service
 @Slf4j
 public class WBLabourNotificationService implements NotificationService {
 
