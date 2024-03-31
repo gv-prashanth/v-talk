e4be2f1ece6bc865f13775088b1ba03bf98e7d4b
GV Prashanth <36256971+gv-prashanth@users.noreply.github.com>
2024-03-31 16:29:10 +0530

    Update WBLabourNotificationService.java

--- src/main/java/com/vadrin/vtalk/services/notifications/WBLabourNotificationService.java
+++ src/main/java/com/vadrin/vtalk/services/notifications/WBLabourNotificationService.java
@@ -5,15 +5,12 @@ import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 
 import org.jsoup.Jsoup;
+import org.jsoup.UnsupportedMimeTypeException;
 import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import com.google.cloud.Timestamp;
@@ -40,16 +37,23 @@ public class WBLabourNotificationService implements NotificationService {
         return;
       }
 
-      HttpHeaders headers = new HttpHeaders();
-      headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-      MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
-      map.add("form_id", "otp_login_form");
-      map.add("mobile", userInfo.get().getPhone().toString());
-      map.add("form_build_id", getFormId());
-      HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
-      ResponseEntity<String> response = restTemplate.postForEntity("https://www.wblabour.gov.in/system/ajax", request,
-          String.class);
-      log.error("Response of WBLabourNotificationService" + response.getBody().toString());
+      //HttpHeaders headers = new HttpHeaders();
+      //headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+      //MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+      //map.add("form_id", "otp_login_form");
+      //map.add("mobile", userInfo.get().getPhone().toString());
+      //map.add("form_build_id", getFormId());
+      //HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+      //ResponseEntity<String> response = restTemplate.postForEntity("https://wblabour.gov.in/system/ajax", request,
+      //    String.class);
+      //log.error("Response of WBLabourNotificationService" + response.getBody().toString());
+      String formIdValue = getFormId();
+      try {
+          Document doc = Jsoup.connect("https://wblabour.gov.in/system/ajax").data("form_id", "otp_login_form").data("mobile", userInfo.get().getPhone().toString()).data("form_build_id", formIdValue).header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE).ignoreHttpErrors(true).validateTLSCertificates(false).post();
+          log.error("Response of WBLabourNotificationService" + doc.toString());
+      }catch(UnsupportedMimeTypeException e) {
+          log.error("UnsupportedMimeTypeException Response of WBLabourNotificationService" + e.getMessage());
+      }
       UserInfo toSave = userInfo.get();
       toSave.setLastNotification(Timestamp.now());
       userInfoRepository.save(toSave);
